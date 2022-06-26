9cf452a8bfad8cbc2cb64b984b2a6c3f6b7fc7e9
Azazel <hites.work0811@gmail.com>
2022-06-26 20:27:10 +0530

    initial test commit

--- routes/example/index.js
+++ routes/example/index.js
@@ -2,6 +2,4 @@
  * @param {App.fastify} fastify
  * @param {object} opts
  */
-export default async function (fastify, opts) {
-  fastify.get("/", async (request, reply) => "this is an example");
-}
+export default async function (fastify, opts) {}
